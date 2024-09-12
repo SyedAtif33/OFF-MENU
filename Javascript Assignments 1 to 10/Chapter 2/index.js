f3f144339e241c827ec60a9f02fc0b8657aeeb61
SyedAtif33 <satif3726@gmail.com>
2024-09-12 16:38:13 +0500

    Add files via upload

---
+++ Javascript Assignments 1 to 10/Chapter 2/index.js
@@ -0,0 +1,21 @@
+var username;
+var myName = "Syed Atif Ahmed";
+var message = "Hello World";
+alert(message);
+var name = "Syed Atif";
+var age = "20 years old";
+var course = "Certified Mobile Application Development";
+
+alert(name);
+alert(age);
+alert(course);
+var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(pizza);
+var email = "satif3726@gmail.com";
+alert("My email address is " + email);
+var book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the Book " + book);
+document.write("Yah! I can write HTML content through JavaScript");
+var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(design);
+document.write(design);
---
+++ Javascript Assignments 1 to 10/Chapter 3/index.js
@@ -0,0 +1,11 @@
+let age = 20;
+alert("I am " + age + " years old");
+let visits = 15;
+alert("You have visited this site " + visits + " times");
+let birthYear = 2004;
+document.write("My birth year is " + birthYear + "<br>Data type of my declared variable is " + typeof(birthYear));
+let visitorName = "Syed Atif";
+let productTitle = "T-shirt(s)";
+let quantity = 5;
+
+document.write(visitorName + " ordered " + quantity + " " + productTitle + " on XYZ Clothing store.");
